e58bd885538b4f676c7893054dc43379627c222f
DEV BATHANI <bathanid888@gmail.com>
2022-03-26 09:57:06 +0530

    Getting started with textfields

---
+++ types_of_textfields/lib/ui/home_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(),
+    );
+  }
+}
