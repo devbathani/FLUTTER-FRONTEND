73f38a0a50f8ca3cc224e15d050856a5ae8cc336
DEV BATHANI <bathanid888@gmail.com>
2022-04-21 11:55:38 +0530

    Project Started

---
+++ types_of_bottom_nav_bar/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:types_of_bottom_nav_bar/ui/home_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomwView(),
+    );
+  }
+}
