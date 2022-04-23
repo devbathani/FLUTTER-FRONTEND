a46d1d8f3a5add8d20c9d0db26ab8221fa019816
DEV BATHANI <bathanid888@gmail.com>
2022-04-24 02:10:55 +0530

    Project setup completed

---
+++ image_picker/lib/Ui/home_view.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+final h = Get.height;
+final w = Get.width;
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: h * 0.08,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: w * 0.08),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "FLutter Frontend",
+                  style: GoogleFonts.poppins(
+                    textStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: w / 22,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: h * 0.01,
+                ),
+                Text(
+                  "Hey there 👋",
+                  style: GoogleFonts.poppins(
+                    textStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: w / 13,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 0.10 * h,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
