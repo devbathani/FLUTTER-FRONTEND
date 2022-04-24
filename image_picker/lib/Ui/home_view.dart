d707c96732e6f9c93b43fa868ea53800300cb2c1
DEV BATHANI <bathanid888@gmail.com>
2022-04-25 00:50:07 +0530

    Completed

--- image_picker/lib/Ui/home_view.dart
+++ image_picker/lib/Ui/home_view.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -12,7 +15,38 @@ class HomeView extends StatefulWidget {
 final h = Get.height;
 final w = Get.width;
 
+File? _image;
+final picker = ImagePicker();
+
 class _HomeViewState extends State<HomeView> {
+  ///Getting Image from camera
+  _imgFromCamera() async {
+    final pickedFile =
+        await picker.pickImage(source: ImageSource.camera, imageQuality: 50);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No Image Selected');
+      }
+    });
+  }
+
+  ///Getting image from gallery
+  _imgFromGallery() async {
+    final pickedFile =
+        await picker.pickImage(source: ImageSource.gallery, imageQuality: 50);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No Image Selected');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +66,7 @@ class _HomeViewState extends State<HomeView> {
                   "FLutter Frontend",
                   style: GoogleFonts.poppins(
                     textStyle: TextStyle(
-                      color: Colors.white,
+                      color: Colors.black87,
                       fontSize: w / 22,
                       fontWeight: FontWeight.w500,
                     ),
@@ -45,17 +79,169 @@ class _HomeViewState extends State<HomeView> {
                   "Hey there 👋",
                   style: GoogleFonts.poppins(
                     textStyle: TextStyle(
-                      color: Colors.white,
+                      color: Colors.black87,
                       fontSize: w / 13,
                       fontWeight: FontWeight.w600,
                     ),
                   ),
                 ),
-                SizedBox(
-                  height: 0.10 * h,
-                ),
               ],
             ),
+          ),
+          SizedBox(
+            height: 0.20 * h,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              InkWell(
+                onTap: _imgFromCamera,
+                child: Container(
+                  height: h * 0.12,
+                  width: w * 0.40,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade300,
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 15,
+                        spreadRadius: 1.5,
+                        color: Colors.grey.shade600,
+                        offset: const Offset(4, 4),
+                      ),
+                      const BoxShadow(
+                        blurRadius: 15,
+                        spreadRadius: 1.5,
+                        color: Colors.white,
+                        offset: Offset(-4, -4),
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(0.02 * w),
+                  ),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Click Image",
+                          style: GoogleFonts.openSans(
+                            textStyle: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 0.05 * w,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: h * 0.01,
+                        ),
+                        Icon(
+                          Icons.camera_alt,
+                          color: Colors.grey,
+                          size: w * 0.08,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: _imgFromGallery,
+                child: Container(
+                  height: h * 0.12,
+                  width: w * 0.40,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade300,
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 15,
+                        spreadRadius: 1.5,
+                        color: Colors.grey.shade600,
+                        offset: const Offset(4, 4),
+                      ),
+                      const BoxShadow(
+                        blurRadius: 15,
+                        spreadRadius: 1.5,
+                        color: Colors.white,
+                        offset: Offset(-4, -4),
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(0.02 * w),
+                  ),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Pick Image",
+                          style: GoogleFonts.openSans(
+                            textStyle: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 0.05 * w,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: h * 0.01,
+                        ),
+                        Icon(
+                          Icons.photo,
+                          color: Colors.grey,
+                          size: w * 0.08,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: h * 0.05,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: h * 0.30,
+                width: w * 0.85,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade300,
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 15,
+                      spreadRadius: 1.5,
+                      color: Colors.grey.shade600,
+                      offset: const Offset(4, 4),
+                    ),
+                    const BoxShadow(
+                      blurRadius: 15,
+                      spreadRadius: 1.5,
+                      color: Colors.white,
+                      offset: Offset(-4, -4),
+                    ),
+                  ],
+                  borderRadius: BorderRadius.circular(0.02 * w),
+                ),
+                child: Center(
+                  child: _image == null
+                      ? Text(
+                          "Your Image",
+                          style: GoogleFonts.openSans(
+                            textStyle: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 0.08 * w,
+                            ),
+                          ),
+                        )
+                      : Image.file(
+                          _image!,
+                          fit: BoxFit.cover,
+                        ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
--- image_picker/lib/main.dart
+++ image_picker/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Image Picker',
       home: HomeView(),
     );
