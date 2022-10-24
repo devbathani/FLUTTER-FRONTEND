b5c03c38d26945dd57942f1f8c4bacbd88429482
devbathani <devbathani@credicxo.com>
2022-10-24 13:01:57 +0530

    Project Setup Complete

---
+++ diwali_ui/lib/presentation/splash-screen/splash_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
