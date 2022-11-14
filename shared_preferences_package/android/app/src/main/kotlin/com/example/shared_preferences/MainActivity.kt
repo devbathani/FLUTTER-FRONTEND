b528757a7bff7275301067b11941c0850d4d5a81
devbathani <devbathani@credicxo.com>
2022-11-14 12:54:23 +0530

    Project Replaced

--- shared_preferences_package/android/app/src/main/kotlin/com/example/shared_preferences/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.shared_preferences
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ shared_prefs_package/android/app/src/main/kotlin/com/example/shared_prefs_package/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shared_prefs_package
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
