6961e8d107572d2417702d4665d5a7690d852208
devbathani <devbathani@credicxo.com>
2022-11-26 18:19:17 +0530

    Project update

--- bottom_model_sheet/android/app/src/main/kotlin/com/example/bottom_model_sheet/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bottom_model_sheet
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ show_bottom_modal_sheet/android/app/src/main/kotlin/com/example/show_bottom_modal_sheet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.show_bottom_modal_sheet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
