eb763bcb704c99e1944cdeff93200e62bc585374
devbathani <devbathani@credicxo.com>
2022-12-11 14:57:36 +0530

    project complete

--- audioplayers_package/lib/home_screen.dart
+++ audioplayers_package/lib/home_screen.dart
@@ -1,7 +1,9 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+  final audioPlayer = AudioPlayer();
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +18,60 @@ class HomeScreen extends StatelessWidget {
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
           ),
+        ),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 200,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: const Center(
+                  child: Text(
+                    "Click to play audio on your device",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 200,
+            ),
+            InkWell(
+              onTap: () async {
+                audioPlayer.audioCache;
+                await audioPlayer.play(
+                  AssetSource("audio.mp3"),
+                );
+              },
+              child: Container(
+                height: 50,
+                width: 160,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Center(
+                  child: Text(
+                    "Play Audio",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- audioplayers_package/lib/main.dart
+++ audioplayers_package/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Audio Player Package',
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
