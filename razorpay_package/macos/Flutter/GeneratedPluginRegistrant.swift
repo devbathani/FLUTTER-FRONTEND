f7a86333a633d78255b41dcb3ad2470489290a0c
devbathani <bathanid888@gmail.com>
2023-05-24 15:55:39 +0530

    Version updates

--- razorpay_package/macos/Flutter/GeneratedPluginRegistrant.swift
+++ razorpay_package/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,7 @@
 import FlutterMacOS
 import Foundation
 
-import package_info_plus_macos
+import package_info_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
